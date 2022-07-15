1137045ece7fa02f2709d75e17eb4b033480998a
Value Raider <ValueRaider@protonmail.com>
2022-07-15 17:01:48 +0100

    Move src/ folder for cleaner load

--- tests/context.py
+++ tests/context.py
@@ -3,7 +3,8 @@
 import sys
 import os
 _parent_dp = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-_src_dp = os.path.join(_parent_dp, "src")
+# _src_dp = os.path.join(_parent_dp, "src")
+_src_dp = _parent_dp
 sys.path.insert(0, _src_dp)
 
 # import yfinance_cache
