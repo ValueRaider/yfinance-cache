37f47e1a7256c4b9bb2b32f0b23789f3c777aaa8
ValueRaider <ValueRaider@protonmail.com>
2022-09-30 19:49:50 +0100

    Initial attempt at fixing PKG

--- make-test.sh
+++ make-test.sh
@@ -8,23 +8,22 @@ _make() (
 	_ver=`cat version-test`
 	_ddir=dist-test
 
+	_outdir="$_ddir/$_ver"
+	_nameu=`echo "$_name" | sed "s/-/_/g"`
+
 	cp setup.cfg.template setup.cfg
 	sed -i "s/<NAME>/$_name/g" setup.cfg
 	sed -i "s/<VERSION>/$_ver/g" setup.cfg
 
-	_nameu=`echo "$_name" | sed "s/-/_/g"`
 	if [ -d src/"$_nameu".egg-info ]; then
 		rm -r src/"$_nameu".egg-info
 	fi
 
-	_outdir="$_ddir/$_ver"
 	if [ -d "$_outdir" ]; then rm -r "$_outdir" ; fi
 	mkdir -p "$_outdir"
 	python -m build --outdir "$_outdir"
+	# python -m build --sdist --wheel --outdir "$_outdir"
 
-	if [ -d src/"$_nameu".egg-info ]; then
-		rm -r src/"$_nameu".egg-info
-	fi
 	python -m twine upload --repository testpypi "$_outdir"/*
 )
 
