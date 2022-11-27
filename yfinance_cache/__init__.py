f111bcc9ae3fee49ea7e8dcca5fe4aa5356cc1f5
ValueRaider <ValueRaider@protonmail.com>
2022-11-27 22:42:56 +0000

    Add scan for incomplete daily+ data to __init__

--- yfinance_cache/__init__.py
+++ yfinance_cache/__init__.py
@@ -6,4 +6,7 @@ from .yfc_ticker import Ticker
 from .yfc_upgrade import _move_cache_dirpath
 _move_cache_dirpath()
 
+from .yfc_upgrade import _prune_incomplete_daily_intervals
+_prune_incomplete_daily_intervals()
+
 # __all__ = ['Ticker', 'Period', 'Interval']
