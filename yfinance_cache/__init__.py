e6177db9566ab21f28d6b718d5c0f77e8df0518e
ValueRaider <ValueRaider@protonmail.com>
2024-08-24 00:16:52 +0100

    pdocs v6

--- yfinance_cache/__init__.py
+++ yfinance_cache/__init__.py
@@ -6,6 +6,20 @@ from .yfc_multi import download
 from .yfc_logging import EnableLogging, DisableLogging
 from .yfc_cache_manager import _option_manager as options
 
+# Define the public API
+__all__ = [
+    'Period',
+    'Interval',
+    'AmbiguousComparisonException',
+    'Ticker',
+    'verify_cached_tickers_prices',
+    'download',
+    'EnableLogging',
+    'DisableLogging',
+    'options'
+]
+
+# Upgrade the data in cache, e.g. fix errors, update structures:
 from .yfc_upgrade import _init_options, _reset_cached_cals
 _init_options()
 _reset_cached_cals()
